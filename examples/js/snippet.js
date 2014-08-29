041148f03c1e3abce1424b48d076e8e0a1258f5f
ken-sanders <ksanders53@gmail.com>
2014-08-29 15:50:32 -0400

    added functions to kill session

--- examples/js/snippet.js
+++ examples/js/snippet.js
@@ -8,7 +8,7 @@
 	generated_script = document.createElement( script );
 	generated_script.async = 1;
 	generated_script.src = script_source;
-	calling_script.parentNode.insertBefore( generated_script, calling_script )
+	calling_script.parentNode.insertBefore( generated_script, calling_script );
 })( document, 'script', '../lib/brytescore.js', window, 'brytescore' );
 brytescore( 'setAPIKey', 'DEMO_API_KEY' );
 brytescore( 'pageview', { "campaign": "some_campaign", "campaignMedium": "some_campaignMedium", "campaignSource": 'some_campaignSource' } );
