14c3d97d4416cb7a66236ada0d7e6a8f6c4604ce
Brady Doll <brady@brytecore.com>
2018-10-03 22:46:48 -0400

    Update package version and copyright date.

--- gruntfile.js
+++ gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function( grunt ) {
 				options: {
 					banner:
 						"/*! Brytescore JavaScript library v<%= pkg.version %>\n" +
-						" *  Copyright 2015-2017 Brytecore, Inc\n" +
+						" *  Copyright 2015-2018 Brytecore, Inc\n" +
 						" */\n"
 				},
 				src: [
